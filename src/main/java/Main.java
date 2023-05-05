33427e335095835026ad6307370a33f18e20495b
Thanh Trong Vo <votrong1471@gmail.com>
2023-05-06 02:05:34 +0700

    upSer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,20 @@
 import DAO.TourDAO;
 import DTO.TourDTO;
+import GUI.Manager;
 
 import java.util.ArrayList;
 
 public class Main{
 	
     public static void main(String[] args) {
-        //    	Manager manager = new Manager();
-        //		manager.setVisible(true);
-        TourDAO dao = new TourDAO();
-        ArrayList<TourDTO> abcs = dao.getAll();
-        for (TourDTO abc : abcs) {
-            String d = abc.getStart_day().toString();
-            System.out.println(d);
-        }
+            	Manager manager = new Manager();
+        		manager.setVisible(true);
+//        TourDAO dao = new TourDAO();
+//        ArrayList<TourDTO> abcs = dao.getAll();
+//        for (TourDTO abc : abcs) {
+//            String d = abc.getStart_day().toString();
+//            System.out.println(d);
+//        }
     }
 }
 
