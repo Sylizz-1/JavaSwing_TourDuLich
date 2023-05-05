522f08f6b9472789bb76e2c5e47b82d445612b40
Sylizz-1 <nguyenhuykhiem2111@gmail.com>
2023-05-06 01:21:02 +0700

    update sql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,19 @@
-import GUI.Manager;
-import GUI.TourContent;
+import DAO.TourDAO;
+import DTO.TourDTO;
 
+import java.util.ArrayList;
 
 public class Main{
 	
-    public static void main(String[] args)  {
-//        TourDAO abc = new TourDAO();
-//
-//        TourDTO tour = abc.getById(1);
-//        double a = tour.getPrice();
-//        System.out.println(Double.valueOf(a));
-
-//        TourDAO abc = new TourDAO();
-//        System.out.println( abc.getClass().getName());
-//        EmpManager employeeManager = new EmpManager();
-//        employeeManager.setVisible(true);
-//     	 ArrayList<HotelDTO> jjjArrayList = HotelDAO.getInstance().getAll();
-//    	 for(HotelDTO ddd: jjjArrayList) {
-//    		 System.out.println(ddd.toString());
-//    	 }
-//    	 HotelDAO.getInstance().delete(13);
-//      	 ArrayList<HotelDTO> jjFDFD = HotelDAO.getInstance().getAll();
-//    	 for(HotelDTO ddd: jjFDFD) {
-//    		 System.out.println(ddd.toString());
-//    	 }
-//    	ArrayList<CustomerDTO> dddArrayList = CustomerDAO.getInstance().getAll();
-//    	for(CustomerDTO dddCustomerDTO : dddArrayList) {
-////    		System.out.println(dddArrayList.toString());
-    	Manager manager = new Manager();
-		manager.setVisible(true);
-
-    	}
-	
-	
+    public static void main(String[] args) {
+        //    	Manager manager = new Manager();
+        //		manager.setVisible(true);
+        TourDAO dao = new TourDAO();
+        ArrayList<TourDTO> abcs = dao.getAll();
+        for (TourDTO abc : abcs) {
+            String d = abc.getStart_day().toString();
+            System.out.println(d);
+        }
     }
+}
 
