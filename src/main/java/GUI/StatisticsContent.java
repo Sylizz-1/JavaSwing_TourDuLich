ba2ba100882551b0b6235fb8fd9f8db25ef84cb2
NamNguyenSGU <102597360+NamNguyenSGU@users.noreply.github.com>
2023-05-10 17:02:41 +0700

    nam

--- src/main/java/GUI/StatisticsContent.java
+++ src/main/java/GUI/StatisticsContent.java
@@ -32,18 +32,18 @@ public class StatisticsContent extends JPanel {
 	private JPanel pnlStatisticsBy;
 	private JPanel panel_4;
 	private JLabel lblBy;
-	private JRadioButton rdbtnByYear;
+	private JRadioButton rdbtnByYear_Booking;
 	private JLabel lblNewLabel_4;
-	private JRadioButton rdbtnByMonth;
+	private JRadioButton rdbtnByMonth_Booking;
 	private JPanel panel_5;
 	private JLabel lblSelectMonth;
-	private JComboBox cbxMonth;
+	private JComboBox cbxMonth_Booking;
 	private JLabel lblSelectYear;
 	private JLabel lblNewLabel_3;
 	private JPanel panel_6;
-	private JButton btnShowChart;
-	private JPanel pnlDetailList;
-	private JComboBox cbxYear;
+	private JButton btnShowChart_Booking;
+	private JPanel pnlDetailBookingList;
+	private JComboBox cbxYear_Booking;
 	private JPanel pnlTourList;
 	private JPanel pnlBookingList;
 	private JPanel pnlCustomerList;
@@ -52,17 +52,34 @@ public class StatisticsContent extends JPanel {
 	private JScrollPane scrollPane_3;
 	private JTable BookingTable;
 	private JTable TourTable;
-	private JTable CustomerTable;
-	
-	private CardLayout cardLayoutDetailList;
 
-	 
-	public JPanel getPnlDetailList() {
-		return pnlDetailList;
+	private JPanel pnlBookingStatistical;
+	private JPanel pnlTourStatistical;
+	private JPanel pnlCusStatistical;
+	private JButton btnView_Booking;
+	private JRadioButton rdbtnByYear_Tour;
+	private JRadioButton rdbtnByMonth_Tour;
+	private JComboBox cbxMonth_Tour;
+	private JComboBox cbxYear_Tour;
+	private JButton btnShowChart_Tour;
+	private JButton btnView_Tour;
+	private JPanel pnlDetailTourList;
+	private JRadioButton rdbtnByYear_Cus;
+	private JRadioButton rdbtnByMonth_Cus;
+	private JComboBox cbxMonth_Cus;
+	private JComboBox cbxYear_Cus;
+	private JButton btnShowChart_Cus;
+	private JButton btnView_Cus;
+	private JPanel pnlDetailCusList;
+	private JTable CusTable;
+	
+	private CardLayout cardLayoutPnlContent;
+	public JPanel getPnlStatisticContent() {
+		return pnlStatisticContent;
 	}
 
-	public void setPnlDetailList(JPanel pnlDetailList) {
-		this.pnlDetailList = pnlDetailList;
+	public void setPnlStatisticContent(JPanel pnlStatisticContent) {
+		this.pnlStatisticContent = pnlStatisticContent;
 	}
 
 	public JPanel getPnlBookingList() {
@@ -105,7 +122,7 @@ public class StatisticsContent extends JPanel {
 		btnStatisticBooking = new JButton("Booking");
 		btnStatisticBooking.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				cardLayoutDetailList.show(pnlDetailList,"pnlBookingList");
+				cardLayoutPnlContent.show(pnlStatisticContent,"pnlBookingStatistical");
 			}
 		});
 		btnStatisticBooking.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -115,7 +132,7 @@ public class StatisticsContent extends JPanel {
 		btnStatisticTour = new JButton("  Tour  ");
 		btnStatisticTour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				cardLayoutDetailList.show(pnlDetailList,"pnlTourList");
+				cardLayoutPnlContent.show(pnlStatisticContent,"pnlTourStatistical");
 			}
 		});
 		btnStatisticTour.setFocusPainted(false);
@@ -125,20 +142,26 @@ public class StatisticsContent extends JPanel {
 		btnStatisticCustomer = new JButton("Customer");
 		btnStatisticCustomer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				cardLayoutDetailList.show(pnlDetailList,"pnlCustomerList");
+				cardLayoutPnlContent.show(pnlStatisticContent,"pnlCustomerStatistical");
 			}
 		});
 		btnStatisticCustomer.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnStatisticCustomer.setFocusPainted(false);
 		pnlStatisticsList.add(btnStatisticCustomer);
 		
+		
 		pnlStatisticContent = new JPanel();
 		add(pnlStatisticContent, BorderLayout.CENTER);
-		pnlStatisticContent.setLayout(new BorderLayout(0, 0));
+		pnlStatisticContent.setLayout(new CardLayout(0, 0));
+		
+		pnlBookingStatistical = new JPanel();
+		pnlStatisticContent.add(pnlBookingStatistical, "pnlBookingStatistical");
+		pnlBookingStatistical.setLayout(new BorderLayout(0, 0));
+		
 		
 		pnlStatisticsBy = new JPanel();
 		pnlStatisticsBy.setPreferredSize(new Dimension(10, 80));
-		pnlStatisticContent.add(pnlStatisticsBy, BorderLayout.NORTH);
+		pnlBookingStatistical.add(pnlStatisticsBy, BorderLayout.NORTH);
 		pnlStatisticsBy.setLayout(new BorderLayout(0, 0));
 		
 		panel_4 = new JPanel();
@@ -149,18 +172,18 @@ public class StatisticsContent extends JPanel {
 		lblBy = new JLabel(" Statistics By             ");
 		panel_4.add(lblBy);
 		
-		rdbtnByYear = new JRadioButton("By year");
-		rdbtnByYear.setFocusPainted(false);
-		rdbtnByYear.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		panel_4.add(rdbtnByYear);
+		rdbtnByYear_Booking = new JRadioButton("By year");
+		rdbtnByYear_Booking.setFocusPainted(false);
+		rdbtnByYear_Booking.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.add(rdbtnByYear_Booking);
 		
 		lblNewLabel_4 = new JLabel("             ");
 		panel_4.add(lblNewLabel_4);
 		
-		rdbtnByMonth = new JRadioButton("By month of the year");
-		rdbtnByMonth.setFocusPainted(false);
-		rdbtnByMonth.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		panel_4.add(rdbtnByMonth);
+		rdbtnByMonth_Booking = new JRadioButton("By month of the year");
+		rdbtnByMonth_Booking.setFocusPainted(false);
+		rdbtnByMonth_Booking.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.add(rdbtnByMonth_Booking);
 		
 		panel_5 = new JPanel();
 		pnlStatisticsBy.add(panel_5, BorderLayout.CENTER);
@@ -169,16 +192,16 @@ public class StatisticsContent extends JPanel {
 		lblSelectMonth = new JLabel("Select Month           ");
 		panel_5.add(lblSelectMonth);
 		
-		cbxMonth = new JComboBox();
-		cbxMonth.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
-		panel_5.add(cbxMonth);
+		cbxMonth_Booking = new JComboBox();
+		cbxMonth_Booking.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		panel_5.add(cbxMonth_Booking);
 		
 		lblSelectYear = new JLabel("                    Select Year            ");
 		panel_5.add(lblSelectYear);
 		
-		cbxYear = new JComboBox();
-		cbxYear.setModel(new DefaultComboBoxModel(new String[] {"2019", "2020", "2021", "2022", "2023"}));
-		panel_5.add(cbxYear);
+		cbxYear_Booking = new JComboBox();
+		cbxYear_Booking.setModel(new DefaultComboBoxModel(new String[] {"2019", "2020", "2021", "2022", "2023"}));
+		panel_5.add(cbxYear_Booking);
 		
 		lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setPreferredSize(new Dimension(92, 14));
@@ -189,22 +212,22 @@ public class StatisticsContent extends JPanel {
 		pnlStatisticsBy.add(panel_6, BorderLayout.SOUTH);
 		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
 		
-		btnShowChart = new JButton("  Chart  ");
-		btnShowChart.setFocusPainted(false);
-		btnShowChart.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		panel_6.add(btnShowChart);
+		btnShowChart_Booking = new JButton("  Chart  ");
+		btnShowChart_Booking.setFocusPainted(false);
+		btnShowChart_Booking.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_6.add(btnShowChart_Booking);
 		
-		pnlDetailList = new JPanel();
-		pnlDetailList.setLayout(new CardLayout(0, 0));
-		pnlDetailList.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Detail List", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		pnlStatisticContent.add(pnlDetailList, BorderLayout.CENTER);
+		btnView_Booking = new JButton("View");
+		btnView_Booking.setFocusPainted(false);
+		panel_6.add(btnView_Booking);
 		
-		pnlBookingList = new JPanel();
-		pnlDetailList.add(pnlBookingList, "pnlBookingList");
-		pnlBookingList.setLayout(new BorderLayout(0, 0));
+		pnlDetailBookingList = new JPanel();
+		pnlDetailBookingList.setLayout(new CardLayout(0, 0));
+		pnlDetailBookingList.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Detail List", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pnlBookingStatistical.add(pnlDetailBookingList, BorderLayout.CENTER);
 		
 		scrollPane_1 = new JScrollPane();
-		pnlBookingList.add(scrollPane_1, BorderLayout.CENTER);
+		pnlDetailBookingList.add(scrollPane_1, BorderLayout.CENTER);
 		
 		Object [][] data14 = {
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
@@ -243,15 +266,83 @@ public class StatisticsContent extends JPanel {
 		BookingTable = new JTable(data14, items14);
 		scrollPane_1.setViewportView(BookingTable);
 		
-		pnlTourList = new JPanel();
-		pnlDetailList.add(pnlTourList, "pnlTourList");
-		pnlTourList.setLayout(new BorderLayout(0, 0));
+		//----------------------------------------------------------
+		pnlTourStatistical = new JPanel();
+		pnlStatisticContent.add(pnlTourStatistical, "pnlTourStatistical");
+		pnlTourStatistical.setLayout(new BorderLayout(0, 0));
+		
+		pnlStatisticsBy = new JPanel();
+		pnlStatisticsBy.setPreferredSize(new Dimension(10, 80));
+		pnlTourStatistical.add(pnlStatisticsBy, BorderLayout.NORTH);
+		pnlStatisticsBy.setLayout(new BorderLayout(0, 0));
+		
+		panel_4 = new JPanel();
+		panel_4.setPreferredSize(new Dimension(10, 25));
+		pnlStatisticsBy.add(panel_4, BorderLayout.NORTH);
+		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.X_AXIS));
+		
+		lblBy = new JLabel(" Statistics By             ");
+		panel_4.add(lblBy);
+		
+		rdbtnByYear_Tour = new JRadioButton("By year");
+		rdbtnByYear_Tour.setFocusPainted(false);
+		rdbtnByYear_Tour.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.add(rdbtnByYear_Tour);
+		
+		lblNewLabel_4 = new JLabel("             ");
+		panel_4.add(lblNewLabel_4);
+		
+		rdbtnByMonth_Tour = new JRadioButton("By month of the year");
+		rdbtnByMonth_Tour.setFocusPainted(false);
+		rdbtnByMonth_Tour.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.add(rdbtnByMonth_Tour);
+		
+		panel_5 = new JPanel();
+		pnlStatisticsBy.add(panel_5, BorderLayout.CENTER);
+		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		lblSelectMonth = new JLabel("Select Month           ");
+		panel_5.add(lblSelectMonth);
+		
+		cbxMonth_Tour = new JComboBox();
+		cbxMonth_Tour.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		panel_5.add(cbxMonth_Tour);
+		
+		lblSelectYear = new JLabel("                    Select Year            ");
+		panel_5.add(lblSelectYear);
+		
+		cbxYear_Tour = new JComboBox();
+		cbxYear_Tour.setModel(new DefaultComboBoxModel(new String[] {"2019", "2020", "2021", "2022", "2023"}));
+		panel_5.add(cbxYear_Tour);
+		
+		lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setPreferredSize(new Dimension(92, 14));
+		panel_5.add(lblNewLabel_3);
+		
+		panel_6 = new JPanel();
+		panel_6.setPreferredSize(new Dimension(10, 30));
+		pnlStatisticsBy.add(panel_6, BorderLayout.SOUTH);
+		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
+		
+		btnShowChart_Tour = new JButton("  Chart  ");
+		btnShowChart_Tour.setFocusPainted(false);
+		btnShowChart_Tour.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_6.add(btnShowChart_Tour);
+		
+		btnView_Tour = new JButton("View");
+		btnView_Tour.setFocusPainted(false);
+		panel_6.add(btnView_Tour);
+		
+		pnlDetailTourList = new JPanel();
+		pnlDetailTourList.setLayout(new CardLayout(0, 0));
+		pnlDetailTourList.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Detail List", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pnlTourStatistical.add(pnlDetailTourList, BorderLayout.CENTER);
 		
 		scrollPane_2 = new JScrollPane();
-		pnlTourList.add(scrollPane_2, BorderLayout.CENTER);
+		pnlDetailTourList.add(scrollPane_2, BorderLayout.CENTER);
 		
 		Object [][] data15 = {
-				{"111", "Thái bình", "Miền Trung", "20", "20","20", "20"},
+				{"111", "Khánh Hòa", "Miền Trung", "20", "20","20", "20"},
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
@@ -284,19 +375,88 @@ public class StatisticsContent extends JPanel {
 		};
 
 		String [] items15 = {"ID", "Name", "Area", "Number of days", "Number of peoples", "Number of peoples", "Number of peoples"};
-		
 		TourTable = new JTable(data15, items15);
 		scrollPane_2.setViewportView(TourTable);
 		
-		pnlCustomerList = new JPanel();
-		pnlDetailList.add(pnlCustomerList, "pnlCustomerList");
-		pnlCustomerList.setLayout(new BorderLayout(0, 0));
+		
+		
+		//----------------------------------------------------------
+		pnlCusStatistical = new JPanel();
+		pnlStatisticContent.add(pnlCusStatistical, "pnlCusStatistical");
+		pnlCusStatistical.setLayout(new BorderLayout(0, 0));
+		
+		pnlStatisticsBy = new JPanel();
+		pnlStatisticsBy.setPreferredSize(new Dimension(10, 80));
+		pnlCusStatistical.add(pnlStatisticsBy, BorderLayout.NORTH);
+		pnlStatisticsBy.setLayout(new BorderLayout(0, 0));
+		
+		panel_4 = new JPanel();
+		panel_4.setPreferredSize(new Dimension(10, 25));
+		pnlStatisticsBy.add(panel_4, BorderLayout.NORTH);
+		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.X_AXIS));
+		
+		lblBy = new JLabel(" Statistics By             ");
+		panel_4.add(lblBy);
+		
+		rdbtnByYear_Cus = new JRadioButton("By year");
+		rdbtnByYear_Cus.setFocusPainted(false);
+		rdbtnByYear_Cus.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.add(rdbtnByYear_Cus);
+		
+		lblNewLabel_4 = new JLabel("             ");
+		panel_4.add(lblNewLabel_4);
+		
+		rdbtnByMonth_Cus = new JRadioButton("By month of the year");
+		rdbtnByMonth_Cus.setFocusPainted(false);
+		rdbtnByMonth_Cus.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.add(rdbtnByMonth_Cus);
+		
+		panel_5 = new JPanel();
+		pnlStatisticsBy.add(panel_5, BorderLayout.CENTER);
+		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		lblSelectMonth = new JLabel("Select Month           ");
+		panel_5.add(lblSelectMonth);
+		
+		cbxMonth_Cus = new JComboBox();
+		cbxMonth_Cus.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		panel_5.add(cbxMonth_Cus);
+		
+		lblSelectYear = new JLabel("                    Select Year            ");
+		panel_5.add(lblSelectYear);
+		
+		cbxYear_Cus = new JComboBox();
+		cbxYear_Cus.setModel(new DefaultComboBoxModel(new String[] {"2019", "2020", "2021", "2022", "2023"}));
+		panel_5.add(cbxYear_Cus);
+		
+		lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setPreferredSize(new Dimension(92, 14));
+		panel_5.add(lblNewLabel_3);
+		
+		panel_6 = new JPanel();
+		panel_6.setPreferredSize(new Dimension(10, 30));
+		pnlStatisticsBy.add(panel_6, BorderLayout.SOUTH);
+		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
+		
+		btnShowChart_Cus = new JButton("  Chart  ");
+		btnShowChart_Cus.setFocusPainted(false);
+		btnShowChart_Cus.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_6.add(btnShowChart_Cus);
+		
+		btnView_Cus = new JButton("View");
+		btnView_Cus.setFocusPainted(false);
+		panel_6.add(btnView_Cus);
+		
+		pnlDetailCusList = new JPanel();
+		pnlDetailCusList.setLayout(new CardLayout(0, 0));
+		pnlDetailCusList.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Detail List", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pnlCusStatistical.add(pnlDetailCusList, BorderLayout.CENTER);
 		
 		scrollPane_3 = new JScrollPane();
-		pnlCustomerList.add(scrollPane_3, BorderLayout.CENTER);
+		pnlDetailCusList.add(scrollPane_3, BorderLayout.CENTER);
 		
 		Object [][] data16 = {
-				{"111", "Khánh hòa", "Miền Trung", "20", "20","20", "20"},
+				{"111", "Thai Binh", "Miền Trung", "20", "20","20", "20"},
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
 				{"111", "Nha Trang", "Miền Trung", "20", "20","20", "20"},
@@ -329,12 +489,10 @@ public class StatisticsContent extends JPanel {
 		};
 
 		String [] items16 = {"ID", "Name", "Area", "Number of days", "Number of peoples", "Number of peoples", "Number of peoples"};
+		CusTable = new JTable(data16, items16);
+		scrollPane_3.setViewportView(CusTable);
 		
-		CustomerTable = new JTable(data16, items16);
-		scrollPane_3.setViewportView(CustomerTable);
-		
-		
-		cardLayoutDetailList =  (CardLayout)(this.getPnlDetailList().getLayout());
+		cardLayoutPnlContent =  (CardLayout)(this.getPnlStatisticContent().getLayout());
 	
 	}
 	
